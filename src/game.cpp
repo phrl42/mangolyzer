53ee29608b36a85095e81fd8cbd0f1a4bc7029ad
phrl42 <phrl42@riseup.net>
2023-04-18 12:32:00 +0200

    did a little trolling

--- src/game.cpp
+++ src/game.cpp
@@ -5,9 +5,9 @@ namespace game
   banana::BananaWindow Game::win;
   void Game::Init()
   {
-    win.h = 100;
-    win.w = 100;
-    win.title = "bomb";
+    win.h = 720;
+    win.w = 1080;
+    win.title = "banana";
     win.color = {1.0f, 1.0f, 1.0f, 0.0f};
 
     banana::Banana::InitBananaWindow(&win);
@@ -15,6 +15,6 @@ namespace game
 
   void Game::Loop(float dt)
   {
-    
+    LOG_GAME("FPS: " + std::to_string(1 / dt));
   }
 };
