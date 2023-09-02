eab0dae2b50b55b97496ab0a6ec035f680299092
phrl42 <phrl42@riseup.net>
2023-09-02 12:08:32 +0200

    need to normalize frequency amplitudes | but fine for now

--- Sandbox/src/scene/layer/FreqLayer.cpp
+++ Sandbox/src/scene/layer/FreqLayer.cpp
@@ -44,10 +44,36 @@ namespace SANDBOX
   {
 
   }
+  float goal[QUADS] = {0};
+  float max_amp = 0;
+  void save()
+  {
+    // also save the highest amplitude in the processed frame
+    for(int i = 0; i < QUADS; i++)
+    {
+      goal[i] = abs(freqs[i]);
+      if(goal[i] > max_amp)
+      {
+	max_amp = goal[i];
+      }
+    }    
+  }
 
   void FreqLayer::OnUpdate(float dt)
   {
     fft(EntryScene::samples, 1, freqs, QUADS);
+
+    float step = 0;
+    for(int i = 0; i < QUADS; i++)
+    {
+      if(goal[i] == abs(freqs[i]))
+      {
+	save();
+      }
+      step = abs(freqs[i]) - goal[i];
+
+      goal[i] += step * (dt * 2);
+    }
 
     float one_width = 2.0f / QUADS;
 
@@ -55,8 +81,8 @@ namespace SANDBOX
     {
       ent[i].transform.proj = Banana::Projection::NONE;
       ent[i].transform.pos = {(i * one_width) - 1, -1, 0};
-      ent[i].transform.size = {one_width, abs(freqs[i]), 0};
-      ent[i].transform.color = {0, 1, 0, 1};
+      ent[i].transform.size = {one_width, goal[i] / 3, 0};
+      ent[i].transform.color = {goal[i], -0.3 + goal[i], 0.8 + goal[i], 1};
       ent[i].Render(dt);
     }
 
