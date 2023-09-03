4c024cfef7b72f0ab34adc7d66df0301d5945dc8
phrl42 <phrl42@riseup.net>
2023-09-03 16:52:56 +0200

    normalized visualizer and fixed NaN bug

--- Sandbox/src/scene/layer/FreqLayer.cpp
+++ Sandbox/src/scene/layer/FreqLayer.cpp
@@ -32,7 +32,6 @@ namespace SANDBOX
     {
       ent[i].AddComponent(new Banana::QuadComponent());
     }
-
     memset(freqs, 0, sizeof(float) * QUADS);
   }
 
@@ -45,34 +44,21 @@ namespace SANDBOX
 
   }
   float goal[QUADS] = {0};
-  float max_amp = 0;
-  void save()
-  {
-    // also save the highest amplitude in the processed frame
-    for(int i = 0; i < QUADS; i++)
-    {
-      goal[i] = abs(freqs[i]);
-      if(goal[i] > max_amp)
-      {
-	max_amp = goal[i];
-      }
-    }    
-  }
 
   void FreqLayer::OnUpdate(float dt)
   {
     fft(EntryScene::samples, 1, freqs, QUADS);
-
+    float max_amp = 1;
     float step = 0;
     for(int i = 0; i < QUADS; i++)
     {
-      if(goal[i] == abs(freqs[i]))
+      if(max_amp < abs(freqs[i]))
       {
-	save();
+	max_amp = abs(freqs[i]);
       }
-      step = abs(freqs[i]) - goal[i];
+      step = ((abs(freqs[i]) / max_amp) * 2) - goal[i];
 
-      goal[i] += step * (dt * 2);
+      goal[i] += step * (dt * 10);
     }
 
     float one_width = 2.0f / QUADS;
@@ -81,7 +67,7 @@ namespace SANDBOX
     {
       ent[i].transform.proj = Banana::Projection::NONE;
       ent[i].transform.pos = {(i * one_width) - 1, -1, 0};
-      ent[i].transform.size = {one_width, goal[i] / 3, 0};
+      ent[i].transform.size = {one_width, goal[i], 0};
       ent[i].transform.color = {goal[i], -0.3 + goal[i], 0.8 + goal[i], 1};
       ent[i].Render(dt);
     }
